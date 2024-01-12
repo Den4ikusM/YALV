23fecb5ca69a1ec262ee4d627287697409a77bfd
Denis Moiseev <denis.moiseev@brighteye.eu>
2024-01-12 10:38:31 +0500

    Momentum.Yalv adapter

---
+++ src/Momentum.YalvAdapter/YalvLayout.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using log4net.Core;
+using log4net.Layout;
+using Newtonsoft.Json;
+using YALV.Core;
+
+namespace Momentum.YalvAdapter
+{
+    internal class YalvLayout : LayoutSkeleton
+    {
+        public override void ActivateOptions()
+        {
+            IgnoresException = false;
+        }
+
+        public override void Format(TextWriter writer, LoggingEvent loggingEvent)
+        {
+            var logEntry = new LogEntry() {
+                Level = loggingEvent.Level.ToString(),
+                TimeStamp = loggingEvent.TimeStamp,
+                Application = loggingEvent.Domain,
+                Thread = loggingEvent.ThreadName,
+                Logger = loggingEvent.LoggerName,
+                Message = loggingEvent.RenderedMessage,
+                Exception = loggingEvent.GetExceptionString(),
+            };
+            var str = JsonConvert.SerializeObject(logEntry);
+            writer.Write(str);
+        }
+    }
+}
---
+++ src/YALV.Core/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YALV.Core
+{
+    struct LogEntry
+    {
+        public string Level { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Application { get; set; }
+        public string Thread { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
